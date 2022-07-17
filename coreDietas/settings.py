44341436fcbc16fd245b365c1fb2b801e47613e7
Mauricio-Macas <55768568+Mauricio-Macas@users.noreply.github.com>
2022-07-17 12:52:48 -0500

    Update settings.py

--- coreDietas/settings.py
+++ coreDietas/settings.py
@@ -65,13 +65,11 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'coreDietas.urls'
 
 
-
+import os
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(BASE_DIR / 'templates'),
-                         ],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
